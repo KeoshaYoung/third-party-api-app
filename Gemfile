2ce09a3d8968c64349fdbc7d5d188c8a001e0dd6
keoshayoung <keoshayoung@email.com>
2022-08-03 05:56:47 -0400

    init

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ group :development do
 end
 
 gem 'http'
+
+gem 'ruby-holidayapi'
--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -2,7 +2,7 @@ class HolidaysController < ApplicationController
   require "http"
 
   def index
-    response = HTTP.get("https://calendarific.com/api/v2/holidays?api_key=API_KEY")
+    response = HTTP.get("https://calendarific.com/api/v2/holidays?api_key=api-key")
     render json: response.parse(:json)
   end
 end
